cf47b18c84d353d6039e3b84f2001fd8a91b4c9c
John Andrés Arévalo Rodríguez <andresseconds@hotmail.com>
2025-04-21 22:27:16 -0500

    Inicio del proyecto JavaBack ATM

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
 public class Main {
+    static int pin = 1234;
     public static void main(String[] args) {
 
-        try {
-            int result = 10/0;
-        } catch (ArithmeticException e) {
-            System.out.println("No se puede dividir por 0");
+        //Variables básicas
+        String accountNumber = "123456789";
+        double balance = 1500.75;
+
+
+        //Array de montos de transacciones
+        int[] transactionAmounts = {200, -100, 50};
+
+        //Operaciones con variables
+        balance += transactionAmounts[0];//Depósito
+        if(balance > 0 && pin == 1234){
+            System.out.println("Acceso concedido");
         }
+
+        //Uso de operadores
+        balance++; //Incremento
+        String status = (balance < 0) ? "Deuda":"Crédito";
+        System.out.println("Estado de cuenta"+ status);
+
+    }
+
+    public boolean authenticateUser(String inputPin){
+        int attempts = 0;
+        while(attempts < 3){
+            if (Integer.parseInt(inputPin)  ==  this.pin) {
+                return true;
+            }else{
+                attempts++;
+                System.out.println("PIN incorrecto. Intento"+attempts+" de 3");
+            }
+        }
+        return false;
     }
 }
